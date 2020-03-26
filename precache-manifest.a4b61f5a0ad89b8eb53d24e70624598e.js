81f5b71a7f32f58df752dd42866dc2ed576c018e
= <=>
2020-03-26 20:37:55 +0400

    Updates

---
+++ precache-manifest.a4b61f5a0ad89b8eb53d24e70624598e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b259ed18b836c9ef07c3baccddd95987",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "215aa33441e583d7db2b",
+    "url": "/react-quiz/static/css/main.d04aba6a.chunk.css"
+  },
+  {
+    "revision": "69b8374591a76a685bf0",
+    "url": "/react-quiz/static/js/2.fbf4095a.chunk.js"
+  },
+  {
+    "revision": "3945d64be4b1204fede605fd2590a29e",
+    "url": "/react-quiz/static/js/2.fbf4095a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215aa33441e583d7db2b",
+    "url": "/react-quiz/static/js/main.70754396.chunk.js"
+  },
+  {
+    "revision": "7f775d81005cc2bd6c56",
+    "url": "/react-quiz/static/js/runtime-main.fbee9deb.js"
+  }
+]);
